da2c88ef175dc6603d81e8760da70f360244e9c2
myllena-oli <myllena.faria@zup.com.br>
2023-10-13 11:17:01 -0300

    Uploading UserModel and application.properties.

--- src/main/java/edu/zupevolution/model/UserModel.java
+++ src/main/java/edu/zupevolution/model/UserModel.java
@@ -1,4 +1,31 @@
 package edu.zupevolution.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_users")
 public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+    @Column(name = "birthday", nullable = false)
+    private Date birthday;
+    @Column(name = "email", nullable = false)
+    private String email;
+    @Column(name = "access_type", nullable = false)
+    @OneToOne(mappedBy = "id_accessType")
+    private Long access_type;
+
 }
