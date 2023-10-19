848e893608f669a5047a59a6158e6779bdc96155
myllena-oli <myllena.faria@zup.com.br>
2023-10-19 15:44:02 -0300

    Uploading "updateStudy" and fixing "createStudy".

--- src/main/java/edu/zupevolution/controller/StudyController.java
+++ src/main/java/edu/zupevolution/controller/StudyController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/zupevolution/study")
 public class StudyController {
@@ -25,5 +27,18 @@ public class StudyController {
     public ResponseEntity<Object> deleteStudy(@PathVariable Long id) {
         ResponseEntity<Object> response = studyService.deleteStudy(id);
         return new ResponseEntity<>(response.getBody(), response.getStatusCode());
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Object> updateStudy(@PathVariable Long id, @RequestBody StudyModel updatedStudy) {
+
+        ResponseEntity<Object> response = studyService.updateStudy(id, updatedStudy);
+
+        if (response.getStatusCode() == HttpStatus.OK) {
+            return new ResponseEntity<>(response.getBody(), HttpStatus.OK);
+        } else if (response.getStatusCode() == HttpStatus.BAD_REQUEST) {
+            return new ResponseEntity<>(response.getBody(), HttpStatus.BAD_REQUEST);
+        } else {
+            return new ResponseEntity<>(response.getBody(), HttpStatus.NOT_FOUND);
+        }
     }
 }
--- src/main/java/edu/zupevolution/service/StudyService.java
+++ src/main/java/edu/zupevolution/service/StudyService.java
@@ -15,9 +15,30 @@ public class StudyService {
     private StudyRepository studyRepository;
 
     public ResponseEntity<Object> createStudy(StudyModel studyModel) {
+
+        if (studyModel.getContent() == null) {
+            return new ResponseEntity<>("O campo 'content' não pode ser nulo.", HttpStatus.BAD_REQUEST);
+        }
+        if (studyModel.getDeadline() == null) {
+            return new ResponseEntity<>("O campo 'deadline' não pode ser nulo.", HttpStatus.BAD_REQUEST);
+        }
+        if (studyModel.getSkills() == null || studyModel.getSkills().isEmpty()) {
+            return new ResponseEntity<>("O campo 'skills' não pode ser nulo ou vazio.", HttpStatus.BAD_REQUEST);
+        }
+        if (studyModel.getGoal() == null) {
+            return new ResponseEntity<>("O campo 'goal' não pode ser nulo.", HttpStatus.BAD_REQUEST);
+        }
+        if (studyModel.getTimeline() == null) {
+            return new ResponseEntity<>("O campo 'timeline' não pode ser nulo.", HttpStatus.BAD_REQUEST);
+        }
+        if (studyModel.getStatus() == null) {
+            return new ResponseEntity<>("O campo 'status' não pode ser nulo.", HttpStatus.BAD_REQUEST);
+        }
+
         studyRepository.save(studyModel);
         return ResponseEntity.status(HttpStatus.CREATED).body("Estudo criado.");
     }
+
     public ResponseEntity<Object> deleteStudy(Long id) {
         Optional<StudyModel> study = studyRepository.findById(id);
 
@@ -28,4 +49,37 @@ public class StudyService {
             return new ResponseEntity<>("Estudo não encontrado.", HttpStatus.NOT_FOUND);
         }
     }
+
+    public ResponseEntity<Object> updateStudy(Long id, StudyModel updatedStudy) {
+        Optional<StudyModel> existingStudy = studyRepository.findById(id);
+
+        if (existingStudy.isPresent()) {
+            StudyModel study = existingStudy.get();
+
+            if (updatedStudy.getContent() != null) {
+                study.setContent(updatedStudy.getContent());
+            }
+            if (updatedStudy.getDeadline() != null) {
+                study.setDeadline(updatedStudy.getDeadline());
+            }
+            if (updatedStudy.getSkills() != null) {
+                study.setSkills(updatedStudy.getSkills());
+            }
+            if (updatedStudy.getGoal() != null) {
+                study.setGoal(updatedStudy.getGoal());
+            }
+            if (updatedStudy.getTimeline() != null) {
+                study.setTimeline(updatedStudy.getTimeline());
+            }
+            if (updatedStudy.getStatus() != null) {
+                study.setStatus(updatedStudy.getStatus());
+            }
+
+            StudyModel updated = studyRepository.save(study);
+            return new ResponseEntity<>(updated, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Estudo não encontrado.", HttpStatus.NOT_FOUND);
+        }
+    }
+
 }
