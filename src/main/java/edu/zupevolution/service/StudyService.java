ac7102a0aaaa7cb608bd574ab92060c35bea85a0
myllena-oli <myllena.faria@zup.com.br>
2023-10-19 11:10:28 -0300

    Uploading createStudy.

---
+++ src/main/java/edu/zupevolution/service/StudyService.java
@@ -0,0 +1,19 @@
+package edu.zupevolution.service;
+
+import edu.zupevolution.model.StudyModel;
+import edu.zupevolution.teste.StudyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudyService {
+    @Autowired
+    private StudyRepository studyRepository;
+
+    public ResponseEntity<Object> createStudy(StudyModel studyModel) {
+        studyRepository.save(studyModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Estudo criado.");
+    }
+}
